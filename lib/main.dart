c0d3d1ea88dc5ba6d77bdfb6cd13cd970cb69e01
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-05-08 19:59:14 +0300

    Экран любимой еды

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:FlutterProject/ui_kit/_ui_kit.dart';
 import 'package:FlutterProject/ui/screens/food_list_screen.dart';
 import 'package:FlutterProject/ui/screens/cart_screen.dart';
+import 'package:FlutterProject/ui/screens/favorite_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
      return MaterialApp(
       title: 'Japan Eat',
       theme: AppTheme.lightTheme,
-      home: const CartScreen(),
+      home: const FavoriteScreen(),
     );
   }
 }
