1870246ed4627ffb15952ab1d003e8145074530d
эрвин бекиров <tom.margan@mail.ru>
2023-05-11 18:24:06 +0300

    вынос навигации с экрана

--- lib/states/food_state.dart
+++ lib/states/food_state.dart
@@ -1,5 +1,12 @@
 class FoodState {
   FoodState._();
   static final _instance = FoodState._();
-  factory FoodState() => _instance;  
+  factory FoodState() => _instance; 
+  int _currentIndex = 0; //Индекс табнавигации.
+  int get currentIndex => _currentIndex;
+
+  Future<void> onTabTap(int index) async {
+    if (_currentIndex == index) return;
+    _currentIndex = index;
+  } 
 }
