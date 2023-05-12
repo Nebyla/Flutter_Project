6a86184493b5ccab2c47143705dbfb279e980c7d
Nebyla <95207952+Nebyla@users.noreply.github.com>
2023-05-12 17:46:18 +0300

    Управление количеством еды

--- lib/ui/screens/food_detail_screen.dart
+++ lib/ui/screens/food_detail_screen.dart
@@ -19,6 +19,18 @@ class FoodDetail extends StatefulWidget {
 class FoodDetailState extends State<FoodDetail> {
   int get foodId => FoodState().selectedFoodId;
   Food get food => FoodState().foodById(foodId);
+  late int _amount = food.quantity;
+
+  void onIncrementTap() {
+    _amount++;
+    setState(() {});
+  }
+
+  void onDecrementTap() {
+    if (_amount == 1) return;
+    _amount--;
+    setState(() {});
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,14 +134,14 @@ class FoodDetailState extends State<FoodDetail> {
                                     ?.copyWith(color: LightThemeColor.accent),
                               ),
                               CounterButton(
-                                onIncrementTap: () {},
-                                onDecrementTap: () {},
+                                onIncrementTap: onIncrementTap,
+                                onDecrementTap: onDecrementTap,
                                 label: Text(
-                                  food.quantity.toString(),
-                                  style:
-                                      Theme.of(context).textTheme.displayLarge,
+                                  _amount.toString(),
+                                  style: Theme.of(context).textTheme.displayLarge,
                                 ),
                               )
+
                             ],
                           ),
                           const SizedBox(height: 15),
